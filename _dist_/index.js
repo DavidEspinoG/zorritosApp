7af6dde5aafe0baa93b066a0e72d4096f9f30c9a
DavidEspinoG <DavidEspinoG@users.noreply.github.com>
2022-10-13 03:00:33 +0000

    deploy: dc41ed33d0c079568ebfc002b9a6119212b74bc9

---
+++ _dist_/index.js
@@ -0,0 +1,31 @@
+import { registerImage } from "./lazy.js";
+const API_URL = 'https://randomfox.ca/floof/';
+const foxContainer = document.querySelector('#images');
+const foxButton = document.querySelector('#foxButton');
+foxButton.addEventListener('click', () => {
+    getFox();
+})
+const cleanButton = document.querySelector('#clean');
+cleanButton.addEventListener('click', () => {
+    cleanFoxContainer();
+})
+async function getFox(){
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    const image = document.createElement('img');
+    image.dataset.src = data.image;
+    image.className = 'foxImage';
+    
+    const div = document.createElement('div');
+    div.append(image)
+    div.className = "p-3"
+    foxContainer.append(div)
+    registerImage(image);
+    if(foxButton.textContent != 'Another zorrito, please'){
+        foxButton.textContent = 'Another zorrito, please';
+    }   
+}
+
+function cleanFoxContainer(){
+    foxContainer.innerHTML = '';
+}
