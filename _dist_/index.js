b5da1942977e3cadc85b9eaf72bbfaf2afd26674
DavidEspinoG <DavidEspinoG@users.noreply.github.com>
2022-10-14 18:55:29 +0000

    deploy: 80fcc93f99681841098f90fa65adad0a89d82d56

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,12 +1,6 @@
 import { registerImage } from "./lazy.js";
 import { generator } from "./idGenerator.js";
 const API_URL = 'https://randomfox.ca/floof/';
-const foxContainer = document.querySelector('#images');
-const foxButton = document.querySelector('#foxButton');
-
-// foxButton.addEventListener('click', () => {
-//     getFox();
-// })
 
 async function getFox(){
     let columnId = generator.next().value;
@@ -22,38 +16,25 @@ async function getFox(){
         imageDiv.style.height = height;
     }
     
-    image.dataset.src = data.image;
+    image.src = data.image;
     image.className = 'foxImage';
     imageDiv.append(image);
     
-    registerImage(image);
-    if(foxButton.textContent != 'Another zorrito, please'){
-        foxButton.textContent = 'Another zorrito, please';
-    }   
+    
     image.addEventListener('load', () => {
         imageDiv.classList.remove('loading-skeleton');
     })
 }
-for(let i = 0; i < 12; i++){
+for(let i = 0; i < 24; i++){
     getFox()
 }
 
-function infiniteScroll(scroll){
-    
-     if(window.innerHeight < scroll){
-        console.log('Im, here') 
-        getFox();
-     }
- }
+
  
-// console.log(window.innerHeight, window.scrollY)
 window.addEventListener('scroll', () => {
-    const body = document.querySelector('body');
-    if( window.innerHeight + window.scrollY >= document.body.scrollHeight){
-        for(let i = 0; i < 8; i++){
+    if( window.innerHeight + window.scrollY >= document.body.scrollHeight - 400){
+        for(let i = 0; i < 12; i++){
             getFox()
         }
-        
     }
-    // console.log(window.innerHeight, window.scrollY, window.scrollHeight)
 })
