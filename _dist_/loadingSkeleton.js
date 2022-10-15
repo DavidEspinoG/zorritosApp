d00fd8718662fab3c2c10b564d37e4597d38a97d
DavidEspinoG <DavidEspinoG@users.noreply.github.com>
2022-10-15 04:38:42 +0000

    deploy: e4cddbdb49e6e17d74c8a822494c4c1bab2eccc4

--- _dist_/loadingSkeleton.js
+++ _dist_/loadingSkeleton.js
@@ -1,4 +1,17 @@
-export async function loadingSkeleton(image, columnId){
-    const div = document.getElementById(`${columnId}`);
-   
+export function loadingSkeleton(foxNumber){
+    let ids = [];
+    let number = 1;
+    for(let  i = 0; i < foxNumber; i++){
+        if (number > 4 ){
+            number = 1;
+        }
+        ids.push(number);
+        number++;
+    }
+    ids.forEach((id) => {
+        let mainDiv = document.getElementById(`${id}`);
+        let skeletonDiv = document.createElement('div');
+        skeletonDiv.className = 'loading-skeleton';
+        mainDiv.append(skeletonDiv);
+    })
 }
